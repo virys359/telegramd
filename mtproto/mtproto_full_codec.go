3a5266a93fcc711456e47511dd38265365dc318b
wubenqi <wubenqi@gmail.com>
2018-02-02 01:20:47 +0800

    Add new mtproto codec

---
+++ mtproto/mtproto_full_codec.go
@@ -0,0 +1,170 @@
+/*
+ *  Copyright (c) 2018, https://github.com/nebulaim
+ *  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mtproto
+
+import (
+	"net"
+	"io"
+	"github.com/golang/glog"
+	"encoding/hex"
+	"github.com/nebulaim/telegramd/base/crypto"
+	"encoding/binary"
+	"fmt"
+)
+
+type MTProtoFullCodec struct {
+	// conn *net.TCPConn
+	stream *AesCTR128Stream
+}
+
+func NewMTProtoFullCodec(conn *net.TCPConn, d *crypto.AesCTR128Encrypt, e *crypto.AesCTR128Encrypt) *MTProtoFullCodec {
+	return &MTProtoFullCodec{
+		// conn:   conn,
+		stream: NewAesCTR128Stream(conn, d, e),
+	}
+}
+
+func (c *MTProtoFullCodec) Receive() (interface{}, error) {
+	var size int
+	var n int
+	var err error
+
+	b := make([]byte, 1)
+	n, err = io.ReadFull(c.stream, b)
+	if err != nil {
+		return nil, err
+	}
+
+	// glog.Info("first_byte: ", hex.EncodeToString(b[:1]))
+	needAck := bool(b[0] >> 7 == 1)
+	_ = needAck
+
+	b[0] = b[0] & 0x7f
+	// glog.Info("first_byte2: ", hex.EncodeToString(b[:1]))
+
+	if b[0] < 0x7f {
+		size = int(b[0]) << 2
+		// glog.Info("size1: ", size)
+	} else {
+		glog.Info("first_byte2: ", hex.EncodeToString(b[:1]))
+		b := make([]byte, 3)
+		n, err = io.ReadFull(c.stream, b)
+		if err != nil {
+			return nil, err
+		}
+		size = (int(b[0]) | int(b[1])<<8 | int(b[2])<<16) << 2
+		// glog.Info("size2: ", size)
+	}
+
+	left := size
+	buf := make([]byte, size)
+	for left > 0 {
+		n, err = io.ReadFull(c.stream, buf[size-left:])
+		if err != nil {
+			return nil, err
+		}
+		// glog.Info("ReadFull2: ", hex.EncodeToString(buf))
+		left -= n
+	}
+
+	// TODO(@benqi): process report ack and quickack
+	// 截断QuickAck消息，客户端有问题
+	if size == 4 {
+		glog.Errorf("Server response error: ", int32(binary.LittleEndian.Uint32(buf)))
+		return nil, fmt.Errorf("Recv QuickAckMessage, ignore!!!!") //  connId: ", c.stream, ", by client ", m.RemoteAddr())
+	}
+
+	authKeyId := int64(binary.LittleEndian.Uint64(buf))
+	var message MessageBase
+	if authKeyId == 0 {
+		message = NewUnencryptedRawMessage()
+		// message.Decode(buf[8:])
+	} else {
+		message = NewEncryptedRawMessage(authKeyId)
+	}
+
+	err = message.Decode(buf[8:])
+	if err != nil {
+		glog.Errorf("decode message error: {%v}", err)
+		return nil, err
+	}
+
+	return message, nil
+}
+
+func (c *MTProtoFullCodec) Send(msg interface{}) error {
+	message, ok := msg.(MessageBase)
+	if !ok {
+		err := fmt.Errorf("msg type error, only UnencryptedRawMessage or EncryptedRawMessage, msg: {%v}", msg)
+		glog.Error(err)
+		return err
+	}
+
+	b, _ := message.Encode()
+
+	sb := make([]byte, 4)
+	// minus padding
+	size := len(b)/4
+
+	if size < 127 {
+		sb = []byte{byte(size)}
+	} else {
+		binary.LittleEndian.PutUint32(sb, uint32(size<<8|127))
+	}
+
+	b = append(sb, b...)
+	_, err := c.stream.Write(b)
+
+	if err != nil {
+		glog.Errorf("Send msg error: %s", err)
+	}
+
+	return err
+}
+
+func (c *MTProtoFullCodec) Close() error {
+	return c.stream.conn.Close()
+}
+
+type AesCTR128Stream struct {
+	conn      *net.TCPConn
+	encrypt *crypto.AesCTR128Encrypt
+	decrypt *crypto.AesCTR128Encrypt
+}
+
+func NewAesCTR128Stream(conn *net.TCPConn, d *crypto.AesCTR128Encrypt, e *crypto.AesCTR128Encrypt) *AesCTR128Stream {
+	return &AesCTR128Stream{
+		conn:    conn,
+		decrypt: d,
+		encrypt: e,
+	}
+}
+
+func (this *AesCTR128Stream) Read(p []byte) (int, error) {
+	n, err := this.conn.Read(p)
+	if err == nil {
+		this.decrypt.Encrypt(p[:])
+		return n, err
+	}
+	return n, err
+}
+
+func (this *AesCTR128Stream) Write(p []byte) (int, error) {
+	this.encrypt.Encrypt(p[:])
+	return this.conn.Write(p)
+}
---
+++ mtproto/mtproto_proxy_codec.go
@@ -0,0 +1,230 @@
+/*
+ *  Copyright (c) 2018, https://github.com/nebulaim
+ *  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mtproto
+
+import (
+	"net"
+	"io"
+	"github.com/golang/glog"
+	"github.com/nebulaim/telegramd/base/crypto"
+	"github.com/nebulaim/telegramd/baselib/net2"
+	"errors"
+)
+
+// 客户端or服务端
+const (
+	CODEC_TYPE_CLIENT = 1	// Client端
+	CODEC_TYPE_SERVER = 2	// Server端
+)
+
+// 协议版本
+const (
+	MTPROTO_ABRIDGED_VERSION = 1		// 删节版本
+	MTPROTO_INTERMEDIATE_VERSION = 2	// 中间版本
+	MTPROTO_FULL_VERSION = 3		// 完整版本
+)
+
+// Transport类型，不支持UDP
+const (
+	TRANSPORT_TCP = 1		// TCP
+	TRANSPORT_HTTP = 2		// HTTP
+	TRANSPORT_UDP = 3		// UDP, @benqi: 未发现有支持UDP的客户端
+)
+
+const (
+	STATE_CONNECTED = iota
+	// STATE_FIRST_BYTE		//
+	// STATE_FIRST_INT32		//
+	// STATE_FIRST_64BYTES		//
+	STATE_DATA				//
+)
+
+const (
+	// Tcp Transport
+	MTPROTO_ABRIDGED_FLAG = 0xef
+	MTPROTO_INTERMEDIATE_FLAG = 0xeeeeeeee
+
+	// Http Transport
+	HTTP_HEAD_FLAG = 0x44414548
+	HTTP_POST_FLAG = 0x54534f50
+	HTTP_GET_FLAG = 0x20544547
+	HTTP_OPTION_FLAG = 0x4954504f
+
+	VAL2_FLAG = 0x00000000
+)
+
+// 服务端MTPProto代理
+// 服务端需要兼容各种协议
+type MTProtoProxy struct {
+	// codecType int 		// codec type
+	// transportType int 	//
+}
+
+//func NewClientMTProtoProxy(transport int) *MTProtoProxy {
+//	return &MTProtoProxy{
+//		codecType: CODEC_TYPE_CLIENT,
+//	}
+//}
+
+func NewMTProtoProxy() *MTProtoProxy {
+	return &MTProtoProxy{
+	}
+}
+
+func (m *MTProtoProxy) NewCodec(conn *net.TCPConn) (net2.Codec, error) {
+	codec := &MTProtoProxyCodec{
+		conn:  conn,
+		State: STATE_CONNECTED,
+	}
+
+	return codec, nil
+}
+
+//type MTProtoCodec interface {
+//
+//}
+
+type MTProtoProxyCodec struct {
+	codecType int // codec type
+	conn      *net.TCPConn
+	State     int
+	codec     net2.Codec
+}
+
+/**
+  Android client code:
+
+	RAND_bytes(bytes, 64);
+	uint32_t val = (bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | (bytes[0]);
+	uint32_t val2 = (bytes[7] << 24) | (bytes[6] << 16) | (bytes[5] << 8) | (bytes[4]);
+	if (bytes[0] != 0xef &&
+		val != 0x44414548 &&
+		val != 0x54534f50 &&
+		val != 0x20544547 &&
+		val != 0x4954504f &&
+		val != 0xeeeeeeee &&
+		val2 != 0x00000000) {
+		bytes[56] = bytes[57] = bytes[58] = bytes[59] = 0xef;
+		break;
+	}
+ */
+func (c* MTProtoProxyCodec) peekCodec() (net2.Codec, error) {
+	// Lookup codec!
+
+	var b_0_1 = make([]byte, 1)
+	_, err := io.ReadFull(c.conn, b_0_1)
+	if err != nil {
+		// glog.Errorf("MTProtoProxyCodec - read b_0_1 error: %v", err)
+		return nil, err
+	}
+
+	if b_0_1[0] == MTPROTO_ABRIDGED_FLAG {
+		// MTPROTO_ABRIDGED_VERSION
+		glog.Warning("MTProtoProxyCodec - mtproto abridged version, impl in the future!!")
+		return nil, errors.New("mtproto abridged version not impl!!")
+	}
+
+	// not abridged version, we'll lookup codec!
+	var b_1_3= make([]byte, 3)
+	_, err = io.ReadFull(c.conn, b_1_3)
+	if err != nil {
+		// glog.Errorf("MTProtoProxyCodec - read b_1_3 error: %v", err)
+		return nil, err
+	}
+
+	// first uint32
+	val := (uint32(b_1_3[2]) << 24) | (uint32(b_1_3[1]) << 16) | (uint32(b_1_3[0]) << 8) | (uint32(b_0_1[0]))
+	if val == HTTP_HEAD_FLAG || val == HTTP_POST_FLAG || val == HTTP_GET_FLAG || val == HTTP_OPTION_FLAG {
+		// http 协议
+		glog.Warning("MTProtoProxyCodec - http data, impl in the future!!")
+		return nil, errors.New("http not impl!!")
+	}
+
+	// an intermediate version
+	if val == MTPROTO_INTERMEDIATE_FLAG {
+		// http 协议
+		glog.Warning("MTProtoProxyCodec - mtproto intermediate version, impl in the future!!")
+		return nil, errors.New("mtproto intermediate version not impl!!")
+	}
+
+	// recv 4~64 bytes
+	var b_4_60= make([]byte, 60)
+	_, err = io.ReadFull(c.conn, b_4_60)
+	if err != nil {
+		glog.Errorf("MTProtoProxyCodec - read b_4_60 error: %v", err)
+		return nil, err
+	}
+
+	val2 := (uint32(b_4_60[3]) << 24) | (uint32(b_4_60[2]) << 16) | (uint32(b_4_60[1]) << 8) | (uint32(b_4_60[0]))
+	if val2 == VAL2_FLAG {
+		// glog.Errorf("MTProtoProxyCodec - mtproto val2 is 0x00000000")
+		return nil, errors.New("mtproto val2 is 0x00000000, who known?")
+	}
+
+	var tmp [64]byte
+	// 生成decrypt_key
+	for i := 0; i < 48; i++ {
+		tmp[i] = b_4_60[51-i]
+	}
+
+	e, err := crypto.NewAesCTR128Encrypt(tmp[:32], tmp[32:48])
+	if err != nil {
+		// glog.Error("NewAesCTR128Encrypt error: %s", err)
+		return nil, err
+	}
+
+	d, err := crypto.NewAesCTR128Encrypt(b_4_60[4:36], b_4_60[36:52])
+	if err != nil {
+		// glog.Error("NewAesCTR128Encrypt error: %s", err)
+		return nil, err
+	}
+
+	d.Encrypt(b_0_1)
+	d.Encrypt(b_1_3)
+	d.Encrypt(b_4_60)
+
+	if b_4_60[52] != 0xef && b_4_60[53] != 0xef && b_4_60[54] != 0xef && b_4_60[55] != 0xef {
+		// glog.Errorf("MTProtoProxyCodec - first 56~59 byte != 0xef")
+		return nil, errors.New("mtproto buf[56:60]'s byte != 0xef!!")
+	}
+
+	return NewMTProtoFullCodec(c.conn, d, e), nil
+}
+
+func (c *MTProtoProxyCodec) Receive() (interface{}, error) {
+	if c.State == STATE_CONNECTED {
+		codec, err := c.peekCodec()
+		if err != nil {
+			glog.Error(err)
+			return nil, err
+		}
+
+		c.codec = codec
+		c.State = STATE_DATA
+	}
+	return c.codec.Receive()
+}
+
+func (c *MTProtoProxyCodec) Send(interface{}) error {
+	return nil
+}
+
+func (c *MTProtoProxyCodec) Close() error {
+	return nil
+}
+
