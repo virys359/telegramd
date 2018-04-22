c36d82d689526329ed36ca9215f57ac3ad1939ad
omid <yumcoder.platform@gmail.com>
2018-04-22 13:22:29 +0430

    fix issue #39

--- baselib/app/app_instance_test.go
+++ baselib/app/app_instance_test.go
@@ -8,30 +8,65 @@
 package app
 
 import (
-	"github.com/golang/glog"
 	"testing"
+	"github.com/golang/glog"
+	"sync"
 )
 
 type NullInstance struct {
-
+	state int
+	m func()
 }
 
 func (e *NullInstance) Initialize() error {
 	glog.Info("null instance initialize...")
+	e.state = 1
 	return nil
 }
 
 func (e *NullInstance) RunLoop() {
-	glog.Info("null run loop...")
-	QuitAppInstance()
+	glog.Info("null run_loop...")
+	e.state = 2
+	e.m()
 }
 
 func (e *NullInstance) Destroy() {
 	glog.Info("null destroy...")
+	e.state = 3
 }
 
-func TestRun(t *testing.T) {
+func TestRun1(t *testing.T) {
 	instance := &NullInstance{}
+	instance.m = func() {
+		QuitAppInstance()
+	}
+
 	DoMainAppInstance(instance)
+
+	result := instance.state
+	expect := 3
+
+	if result != expect {
+		t.Error(`expect:`, expect, `result:`, result)
+	}
 }
 
+func TestRun2(t *testing.T) {
+	instance := &NullInstance{}
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	instance.m = func() {
+		glog.Info("done...")
+		wg.Done()
+	}
+
+	go DoMainAppInstance(instance)
+	wg.Wait()
+	result := instance.state
+	expect := 2
+
+	if result != expect {
+		t.Error(`expect:`, expect, `result:`, result)
+	}
+}
