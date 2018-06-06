0fc1ca74df3d4637f5bdedc6d819c8f6bd6de701
wubenqi <wubenqi@gmail.com>
2018-06-06 22:57:32 +0800

    Fix data too long for column 'mime_type' bug.

--- scripts/nebulaim.sql
+++ scripts/nebulaim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-06-06 11:01:12
+-- Generation Time: 2018-06-06 14:54:12
 -- 服务器版本： 5.6.37
 -- PHP Version: 5.6.30
 
@@ -357,7 +357,7 @@ CREATE TABLE `documents` (
   `file_size` int(11) NOT NULL,
   `uploaded_file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `ext` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `mime_type` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `mime_type` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `thumb_id` bigint(20) NOT NULL DEFAULT '0',
   `version` int(11) NOT NULL DEFAULT '0',
   `attributes` text COLLATE utf8mb4_unicode_ci NOT NULL,
